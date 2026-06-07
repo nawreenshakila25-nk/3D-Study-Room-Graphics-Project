da9e19de7469d68b59ebd81b8c8170b110c24780
Shakila Khan Nawreen <shakilanowreen@gmail.com>
2026-06-07 22:55:54 +0600

    Implement light toggle functionality
    Added a function to toggle lighting on and off.

--- lighting.cpp
+++ lighting.cpp
@@ -1,1 +1,22 @@
+#include <GL/glut.h>
 
+bool lightOn = true;
+
+/* Toggle Light */
+void toggleLight()
+{
+    lightOn = !lightOn;
+
+    if(lightOn)
+    {
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+    }
+    else
+    {
+        glDisable(GL_LIGHTING);
+        glDisable(GL_LIGHT0);
+    }
+
+    glutPostRedisplay();
+}
