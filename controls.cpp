689954df3a3b605f51c90c7da31741cde431f1a6
Shakila Khan Nawreen <shakilanowreen@gmail.com>
2026-06-07 22:56:19 +0600

    Add keyboard controls for light toggling and exit

--- controls.cpp
+++ controls.cpp
@@ -1,1 +1,19 @@
+#include <GL/glut.h>
 
+/* Function from lighting.cpp */
+void toggleLight();
+
+/* Keyboard controls */
+void keyboard(unsigned char key, int x, int y)
+{
+    switch(key)
+    {
+        case 'l':
+            toggleLight();
+            break;
+
+        case 'q':
+            exit(0);
+            break;
+    }
+}
