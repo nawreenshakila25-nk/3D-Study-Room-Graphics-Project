0c3247ee01b5b34eb2c9115cbb2f38923fee68c2
Shakila Khan Nawreen <shakilanowreen@gmail.com>
2026-06-07 22:54:28 +0600

    Refactor display function and add necessary declarations
    Added function declarations and updated display function for animation.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,38 @@
 #include <GL/glut.h>
 
-void display() {
+/* Functions from other files */
+void displayAnimation();
+void update(int value);
+void keyboard(unsigned char key, int x, int y);
+
+/* Main display */
+void display()
+{
     glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
+
+    displayAnimation();
+
+    glutSwapBuffers();
 }
 
-int main(int argc, char** argv) {
+/* Main function */
+int main(int argc, char** argv)
+{
     glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+    glutInitWindowSize(800, 600);
+
     glutCreateWindow("3D Study Room");
+
     glutDisplayFunc(display);
+
+    glutKeyboardFunc(keyboard);
+
+    glutTimerFunc(16, update, 0);
+
     glutMainLoop();
+
+    return 0;
 }
