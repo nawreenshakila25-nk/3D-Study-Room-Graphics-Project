2277bb3fd0589ef8279cc2096a19e14f6414a7a0
2023100000354-a11y <2023100000354@seu.edu.bd>
2026-06-09 20:15:28 +0600

    Update room.h

--- room.h
+++ room.h
@@ -1,1 +1,28 @@
+#ifndef ROOM_H
+#define ROOM_H
+#include <GL/glut.h>
 
+
+extern GLuint floorTex, outWallTex, inWallTex, woodTex, windowTex, pcTex, clockTex, doorTex, sofaTex, bedsheetTex, emonTex, ranaTex, rugTex, keyboardTex, mouseTex;
+
+
+extern bool lightOn;
+extern bool fanOn;
+extern bool pcOn;
+extern bool acOn;
+extern bool projLightOn; 
+extern bool lampOn;      
+
+
+extern bool doorOpen;
+extern float doorAngle;
+
+
+extern float acFanAngle;
+extern float mistOffset;
+
+void drawRoom();
+void initRoom();
+void reshapeRoom(int w, int h);
+
+#endif
