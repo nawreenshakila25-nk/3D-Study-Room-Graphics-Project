f78a61471f17130d87c30ab8753ec0db522fb4db
2023100000354-a11y <2023100000354@seu.edu.bd>
2026-06-09 20:15:57 +0600

    Update furniture.h

--- furniture.h
+++ furniture.h
@@ -1,1 +1,12 @@
+#ifndef FURNITURE_H
+#define FURNITURE_H
 
+void solidCube(float size); 
+
+void drawTable();
+void drawChair();
+void drawBookshelf();
+void drawDesktopSetup();
+void drawAllFurniture();
+
+#endif 
