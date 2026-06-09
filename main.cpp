d1e8b0a8b3c674e985de8c47d50c1b6875ca902a
2023100000354-a11y <2023100000354@seu.edu.bd>
2026-06-09 20:14:53 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,45 @@
 #include <GL/glut.h>
+#include "room.h"
 
-/* Functions from other files */
+
 void displayAnimation();
 void update(int value);
 void keyboard(unsigned char key, int x, int y);
 
-/* Main display */
+
 void display()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+
+    drawRoom();
+
 
     displayAnimation();
+
 
     glutSwapBuffers();
 }
 
-/* Main function */
+
 int main(int argc, char** argv)
 {
+
     glutInit(&argc, argv);
 
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 
-    glutInitWindowSize(800, 600);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
-    glutCreateWindow("3D Study Room");
+
+    glutInitWindowSize(1000, 700);
+    glutCreateWindow("3D Study Room - Complete Team Project");
+
+
+    initRoom();
+
 
     glutDisplayFunc(display);
-
+    glutReshapeFunc(reshapeRoom);
     glutKeyboardFunc(keyboard);
-
     glutTimerFunc(16, update, 0);
-
     glutMainLoop();
 
     return 0;
