5195f97aac89c30333e7fb781bb6cab4203ad59f
2023100000354-a11y <2023100000354@seu.edu.bd>
2026-06-10 00:01:55 +0600

    Update room.h

--- room.h
+++ room.h
@@ -2,10 +2,10 @@
 #define ROOM_H
 #include <GL/glut.h>
 
+// Texture Variables
+extern GLuint floorTex, outWallTex, inWallTex, woodTex, windowTex, pcTex, clockTex, doorTex, sofaTex, bedsheetTex, emonTex, ranaTex, rugTex, keyboardTex, mouseTex, curtainTex;
 
-extern GLuint floorTex, outWallTex, inWallTex, woodTex, windowTex, pcTex, clockTex, doorTex, sofaTex, bedsheetTex, emonTex, ranaTex, rugTex, keyboardTex, mouseTex;
-
-
+// Appliance & Light States
 extern bool lightOn;
 extern bool fanOn;
 extern bool pcOn;
@@ -13,13 +13,19 @@ extern bool acOn;
 extern bool projLightOn; 
 extern bool lampOn;      
 
-
+// Door States
 extern bool doorOpen;
 extern float doorAngle;
 
-
+// AC Animation States
 extern float acFanAngle;
 extern float mistOffset;
+
+// NEW: Window & Curtain States
+extern bool curtainOpen;
+extern float curtainOffset;
+extern bool windowOpen;
+extern float windowOffset;
 
 void drawRoom();
 void initRoom();
